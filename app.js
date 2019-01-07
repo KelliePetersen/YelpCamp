ebc60736d6d12ce05fc127fd65c37fca7ad8d8cc
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-01-07 00:28:07 -0500

    added registration page

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const express = require("express"),
       mongoose = require("mongoose"),
       Campground = require("./models/campground"),
       Comment = require("./models/comment"),
-      seedDB = require("./seeds");
+      seedDB = require("./seeds"),
+      passport = require('passport'),
+      LocalStrategy = require('passport-local'),
+      User = require('./models/user');
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+app.use(require('express-session')({
+  secret: 'I love my ewey',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("landing");
@@ -78,6 +92,23 @@ app.post("/campgrounds/:id/comments", function(req, res) {
     }
   });
 });
+
+app.get('/register', function(req, res) {
+  res.render('register');
+})
+
+app.post('/register', function(req, res) {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user) {
+    if(err) {
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req, res, function(){
+      res.redirect('/campgrounds');
+    });
+  });
+})
 
 app.listen(3000, function() {
   console.log("server has started");
